0019150d0967f1ab8051c131e87f129486db1a94
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-08-14 18:40:18 -0300

    - App component on components folder - Add Swicth/Redirect/Route - Import new components

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect, Switch } from "react-router-dom";
+import './App.css';
+// The below components will be created shortly
+import Login from "../Login/Login";
+import Groupchat from "../Groupchat/Groupchat";
+
+const App = (props) => {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/chat" component={Groupchat} />
+    </Switch>
+  );
+}
+
+export default App;
