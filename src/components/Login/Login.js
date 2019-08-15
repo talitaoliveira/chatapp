78e5555c9d997f27e87ce6e091e3898a1f3d3880
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-08-15 14:17:16 -0300

    Add functions to login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import * as chat from "../../lib/chat";
+
 
 const Login = (props) => {
+
+  const [username, setUsername] = useState('');
+  const [isAutenticated, setIsAutenticated] = useState(false);
+  const [user, setUser] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    if (username !== '') {
+      e.preventDefault();
+      login();
+    }
+  }
+
+  const login = () => {
+    chat.login(username).then( user => {
+      setUser({isAuthenticated: true})
+    }).catch( error => {
+      setErrorMessage("Please enter a valid username");
+      console.log(error)
+    })
+  }
+
+  const handleInputChange = (e) => {
+    setUsername(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInputChange} />
+        <span>{errorMessage}</span>
+        <input type="submit" disabled={username === ""} value="LOGIN" />
+      </form>
     </div>
   );
 }
